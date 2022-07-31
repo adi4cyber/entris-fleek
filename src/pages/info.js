172203ee1aa40173893b6fab1fd1558eda394575
Adi Prayitno <adi.prayitno@icloud.com>
2022-07-31 13:32:20 +0700

    Update info.js

--- src/pages/info.js
+++ src/pages/info.js
@@ -12,25 +12,6 @@ export default function Info() {
           <div dangerouslySetInnerHTML={{__html: infoData.description}}></div>
           <div dangerouslySetInnerHTML={{__html: infoData.cta}}></div>
         </h2>
-        <ul>
-          <li>
-            <p>
-              <a href={`mailto:${infoData.contact.email}`}>Email: {infoData.contact.email}</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://twitter.com/${infoData.contact.twitter_handle}`}>
-                Twitter: @{infoData.contact.twitter_handle}
-              </a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a href={`https://github.com/${infoData.contact.github_handle}`}>Github: {infoData.contact.github_handle}</a>
-            </p>
-          </li>
-        </ul>
       </section>
     </Layout>
   )
